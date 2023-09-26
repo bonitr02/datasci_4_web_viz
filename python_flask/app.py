6bc1a4ef9b9f589798f38ff317f708495e318288
bonitr02 <rianne.bonitto@gmail.com>
2023-09-26 23:28:15 +0000

    updated all files

--- python_flask/app.py
+++ python_flask/app.py
@@ -12,7 +12,7 @@ warnings.simplefilter("ignore", UserWarning)
 app = Flask(__name__)
 
 # Load the dataset
-url = "https://raw.githubusercontent.com/bonitr02/datasci_4_web_viz/main/HEALTHYPLACES__Local_Data_for_Better_Health__County_Data_2023_release.csv"
+url = "https://raw.githubusercontent.com/bonitr02/datasci_4_web_viz_1/main/datasets/HEALTHYPLACES__Local_Data_for_Better_Health__County_Data_2023_release.csv"
 df = pd.read_csv(url)
 df_teeth = df[(df['MeasureId'] == 'TEETHLOST') & (df['Data_Value_Type'] == 'Age-adjusted prevalence')]
 
